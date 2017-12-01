46894f9699e922f8a5996a1fe98b90181e960b08
Luca Tansini <luca.tansini@studenti.unimi.it>
2017-12-01 17:31:44 +0100

    Fatti progressi con mylist, ma non ancora finito

--- MyList/MyList.py
+++ MyList/MyList.py
@@ -1,12 +1,18 @@
-import sys,traceback
+import inspect
 
 class MyList():
     def __init__(self,n):
         self.value = n
 
     def __del__(self):
-        print(self.value)
-        traceback.print_tb(limit=-10)
-
-    def __set__(self):
-        pass
+        for f in inspect.getouterframes(inspect.currentframe()):
+            print(f,end="\n\n")
+            frame = f[0]
+            print("*****f_code*****\n")
+            print(frame.f_code, end="\n\n")
+            print("*****f_locals*****\n")
+            print(frame.f_locals, end="\n\n")
+            frame.f_locals['l']=42
+            #print("*****f_globals*****\n")
+            #print(frame.f_globals, end="\n\n")
+            print("*"*80+"\n")
