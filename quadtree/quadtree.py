4dbec4fdd7eefd96740766d12838b58a6509320f
Luca Tansini <luca.tansini@studenti.unimi.it>
2017-12-01 19:02:19 +0100

    Fatti progressi con la stampa dell'immagine, ma funziona solo fino a 16*16

--- quadtree/quadtree.py
+++ quadtree/quadtree.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def buildTree(quads):
     for i in range(4):
         c = getQuadColor(quads[i])
@@ -38,3 +40,61 @@ def toQuads(m):
 class quadtree:
     def __init__(self,img):
         self.tree = buildTree(toQuads(img))
+        self.size = len(img)
+
+    def treerepr(self):
+        return repr(self.tree)
+
+    def getimage(self):
+        flatquads = self.intgetimage(self.tree,self.size//4)
+        rows = toQuads(flatquads)
+        img = []
+        for r in rows:
+            img += r
+        return img
+
+    def intgetimage(self,quad,quadsize):
+        out = []
+        for q in quad:
+            if(q == "White"):
+                out += [0]*quadsize
+            elif(q == "Black"):
+                out += [1]*quadsize
+            else:
+                out += self.intgetimage(q,quadsize//4)
+        #print("DEBUG:\n",out)
+        return out
+
+
+import random
+size = 64
+f = True
+count = 0
+while(f and count < 10000):
+    img = []
+    for i in range(size): img+=[random.choice([0,1])]
+    q = quadtree(img)
+    f = img==q.getimage()
+    count+=1
+
+if(not f):
+    print("*"*87)
+    print("count:",count)
+    print("\nimg:")
+    print(img)
+
+    print("\nimg matrix repr:")
+    m = []
+    for i in range(int(sqrt(len(img)))):
+        m+=[(img[i*(int(sqrt(len(img)))):i*(int(sqrt(len(img))))+int(sqrt(len(img)))])]
+    for r in m: print(r)
+
+    q = quadtree(img)
+    print("\ntreerepr:")
+    for i in q.tree: print(i)
+    print("\nimg == q.getimage() ?",img==q.getimage())
+    print("\ngetimage:")
+    print(q.getimage())
+    print("\nintgetimage:")
+    print(q.intgetimage(q.tree,q.size//4))
+    print("*"*87)
