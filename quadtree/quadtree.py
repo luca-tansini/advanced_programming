8f2bf875b67a23d2b4dce71d7fbb143fdd5a9b16
Luca Tansini <luca.tansini@studenti.unimi.it>
2017-12-02 15:17:11 +0100

    Finito esercizio quadtree

--- quadtree/quadtree.py
+++ quadtree/quadtree.py
@@ -1,5 +1,14 @@
 from math import sqrt
 
+def deepcount(l):
+    count = 0
+    for i in l:
+        if isinstance(i,list):
+            count += deepcount(i)
+        else:
+            count += 1
+    return count
+
 def buildTree(quads):
     for i in range(4):
         c = getQuadColor(quads[i])
@@ -17,12 +26,14 @@ def getQuadColor(q):
         return "White"
     return "Grey"
 
+def torows(l):
+    n = int(sqrt(len(l)))
+    return [l[i*n:i*n+n] for i in range(n)]
+
 def toQuads(m):
     n = int(sqrt(len(m)))
-    l=[]
+    l=torows(m)
     ret = [[],[],[],[]]
-    for i in range(int(sqrt(len(m)))):
-        l+=[(m[i*(int(sqrt(len(m)))):i*(int(sqrt(len(m))))+int(sqrt(len(m)))])]
     for i in range(0,int(n/2)):
         for j in range(0,int(n/2)):
             ret[0]+=[l[i][j]]
@@ -37,7 +48,14 @@ def toQuads(m):
             ret[3]+=[l[i][j]]
     return ret
 
+def flatten(l):
+    out = []
+    for e in l:
+        out += e
+    return out
+
 class quadtree:
+
     def __init__(self,img):
         self.tree = buildTree(toQuads(img))
         self.size = len(img)
@@ -46,55 +64,63 @@ class quadtree:
         return repr(self.tree)
 
     def getimage(self):
-        flatquads = self.intgetimage(self.tree,self.size//4)
-        rows = toQuads(flatquads)
-        img = []
-        for r in rows:
-            img += r
-        return img
+        sidesize = int(self.size**0.5)
+        img = [[0]*sidesize for i in range(sidesize)]
+        self.intgetimage(self.tree,sidesize//2,0,0,img)
+        return flatten(img)
 
-    def intgetimage(self,quad,quadsize):
-        out = []
-        for q in quad:
-            if(q == "White"):
-                out += [0]*quadsize
-            elif(q == "Black"):
-                out += [1]*quadsize
+    def intgetimage(self,quads,quadsize,xstart,ystart,img):
+        '''quads è la lista di quadranti, quadsize è la dimensione del lato di un quadrante a questo livello, xstart e ystart sono gli indici di inizio del primo quadrante, img è l'immagine che viene costruita'''
+
+        for q in range(4):
+            qxstart = xstart + quadsize * (q%2)
+            qystart = ystart + quadsize * (q//2)
+            if(isinstance(quads[q],str)):
+                for i in range(qystart,qystart+quadsize):
+                    for j in range(qxstart,qxstart+quadsize):
+                        img[i][j] = 0 if quads[q] == 'White' else 1
             else:
-                out += self.intgetimage(q,quadsize//4)
-        #print("DEBUG:\n",out)
-        return out
+                self.intgetimage(quads[q],quadsize//2,qxstart,qystart,img)
 
+    def calculatesavedspace(self):
+        print("original size: ",self.size)
+        print("compressed size: ",deepcount(self.tree))
+        print("saved space: ",self.size - deepcount(self.tree))
+        return self.size - deepcount(self.tree)
 
 import random
-size = 64
-f = True
-count = 0
-while(f and count < 10000):
-    img = []
-    for i in range(size): img+=[random.choice([0,1])]
+
+def testsave(size):
+    img = [random.choice([0,1]) for i in range(size)]
     q = quadtree(img)
-    f = img==q.getimage()
-    count+=1
+    return q.calculatesavedspace()
 
-if(not f):
-    print("*"*87)
-    print("count:",count)
-    print("\nimg:")
-    print(img)
 
-    print("\nimg matrix repr:")
-    m = []
-    for i in range(int(sqrt(len(img)))):
-        m+=[(img[i*(int(sqrt(len(img)))):i*(int(sqrt(len(img))))+int(sqrt(len(img)))])]
-    for r in m: print(r)
+def test(size,reps=1000):
+    f = True
+    count = 0
 
-    q = quadtree(img)
-    print("\ntreerepr:")
-    for i in q.tree: print(i)
-    print("\nimg == q.getimage() ?",img==q.getimage())
-    print("\ngetimage:")
-    print(q.getimage())
-    print("\nintgetimage:")
-    print(q.intgetimage(q.tree,q.size//4))
-    print("*"*87)
+    while(f and count < reps):
+        img = [random.choice([0,1]) for i in range(size)]
+        q = quadtree(img)
+        f = img==q.getimage()
+        count+=1
+
+    if(not f):
+        print("*"*87)
+        print("count:",count)
+        print("\nimg:")
+        print(img)
+
+        print("\nimg matrix repr:")
+        for r in torows(img): print(r)
+
+        q = quadtree(img)
+        print("\ntreerepr:")
+        for i in q.tree: print(i)
+        print("\ngetimage:")
+        img = q.getimage()
+        print(quadtree.torows(img))
+        print("*"*87)
+    else:
+        print("TEST OK")
