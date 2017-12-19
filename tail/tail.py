130fec772a8bab910ec57bb696eeb7f796bc3e14
Luca Tansini <luca.tansini@studenti.unimi.it>
2017-12-19 14:32:39 +0100

    funziona, ma il decoratore crea funzioni da lanciare con un trampolino esterno

--- tail/tail.py
+++ tail/tail.py
@@ -1,34 +1,60 @@
 import sys
 
+class TailRecursionException(Exception):
+    def __init__(self,*args):
+        self.args=args
+
+#DECORATORE PER LA TAIL RECURSION
 def tail_recursive(func):
     def wrapped(*args):
-        #while True:
-        try:
-            print("\nargs:",*args)
-            frame = sys._getframe()
-            print(frame)
-            print(frame.f_code)
-            print(frame.f_back)
-            print(frame.f_back.f_code)
-            if(frame.f_back.f_back):
-                print(frame.f_back.f_back)
-                print(frame.f_back.f_back.f_code)
-            #Se i due stack frame hanno lo stesso codice sono due record della stessa procedura, quindi sollevo un'eccezione (di tipo TailRecursionException per salvare i parametri) che mi permette di cancellare il record di attivazione corrente
-            if(frame.f_back.f_back and frame.f_code == frame.f_back.f_back.f_code):
-                print("chiamata ricorsiva! spilo...",end="")
-                raise TailRecursionException(*args)
-            return func(*args)
-        except TailRecursionException as e:
-            print("intercettata chiamata ricorsiva con parametri:",*e.args)
-            return func(*e.args)
+        frame = sys._getframe()
+        if(frame.f_back.f_back and frame.f_code == frame.f_back.f_back.f_code):
+            #intercettata chiamata ricorsiva...
+            raise TailRecursionException(*args)
+        return func(*args)
     return wrapped
 
-@tail_recursive
+#FUNZIONE TRAMPOLINO PER CHIAMARE LE FUZIONI DECORATE
+def tail(func,*args):
+    while True:
+        try:
+            return func(*args)
+        except TailRecursionException as e:
+            args=e.args
+
 def fact(out,n):
     if(n<3):
         return out
     return fact(out*(n-1),n-1)
 
-class TailRecursionException(Exception):
-    def __init__(self,*args):
-        self.args=args
+@tail_recursive
+def trfact(out,n):
+    if(n<3):
+        return out
+    return trfact(out*(n-1),n-1)
+
+def fib(i,a,b):
+    if(i==0): return a
+    return fib(i-1,b,a+b)
+
+@tail_recursive
+def trfib(i,a,b):
+    if(i==0): return a
+    return trfib(i-1,b,a+b)
+
+if __name__ == '__main__':
+    print("fib(5000,1,1):")
+    try:
+        fib(5000,1,1)
+    except Exception as e:
+        print(e)
+    print("\ntail(trfib,5000,1,1):")
+    print(tail(trfib,5000,1,1))
+
+    print("\nfact(5000,5000):")
+    try:
+        fact(5000,5000)
+    except Exception as e:
+        print(e)
+    print("\ntail(trfact,5000,5000):")
+    print(str(tail(trfact,5000,5000))[:1000]+"...")
